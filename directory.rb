99918517ccff574ab676798b99d907ba30aedfe9
Lauren Rosie <laurenrosiewatson@gmail.com>
2016-12-08 14:42:20 +0000

    Made students.csv the default file if none given in command line

--- directory.rb
+++ directory.rb
@@ -128,8 +128,9 @@ def load_students(filename = "students.csv")
 end
 
 def try_load_students
-  filename = ARGV.first
-  return if filename.nil?
+
+  ARGV.first.nil? ? filename = "students.csv" : filename = ARGV.first
+
   if File.exists?(filename)
     load_students(filename)
     puts "Loaded #{@students.count} from #{filename}"
